73b6e380d72b43472b61de1d06e6741aaeb8d8ff
Blake Nall <blake@proton.me>
2024-07-01 10:30:48 -0500

    Removed unnecessary import from settings

--- dforms/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("forms/<str:form_name>/", views.get_form, name="form"),
-    path("fields/<str:field_name>/", views.get_field, name="field"),
-]
--- terminusgps/settings.py
+++ terminusgps/settings.py
@@ -1,8 +1,6 @@
 import os
 from pathlib import Path
 
-from intuitlib.enums import Scopes
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
