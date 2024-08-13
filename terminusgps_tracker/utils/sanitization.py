359a1c854e2f4c685740514790d4bde792078bdb
Blake Nall <blake@proton.me>
2024-08-13 13:05:30 -0500

    Removed unnecessary modules

--- terminusgps_tracker/utils/sanitization.py
+++
@@ -1,7 +0,0 @@
-from bleach import clean
-
-from .markdown_config import ALLOWED_ATTRS, ALLOWED_TAGS
-
-def sanitize_html_content(html_content: str) -> str:
-    """Sanitizes HTML content based on the ALLOWED_ATTRS and ALLOWED_TAGS configuration."""
-    return clean(html_content, attributes=ALLOWED_ATTRS, tags=ALLOWED_TAGS)
