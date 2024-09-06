2c0de1d7f2e4007c0b80b0de97372e4ac91e349b
Blake Nall <blake@proton.me>
2024-09-06 12:19:50 -0500

    Fixed urlconf

--- terminusgps/urls.py
+++ terminusgps/urls.py
@@ -5,13 +5,13 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/docs/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("payments/", include("payments.urls")),
-    path("api/", include("rest_framework.urls")),
     path("", include("terminusgps_tracker.urls")),
 ]
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns.append(path("__reload__/", include("django_browser_reload.urls")))
+    urlpatterns.append(path("accounts/", include("django.contrib.auth.urls")))
+    urlpatterns.append(path("payments/", include("payments.urls")))
+    urlpatterns.append(path("api/", include("rest_framework.urls")))
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
