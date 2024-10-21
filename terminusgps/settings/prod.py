e544e73bcee83803039ae0e1aade7509a543a1e7
darthnall <blake@proton.me>
2024-10-21 11:51:11 -0500

    Added logging to production

--- terminusgps/settings/prod.py
+++ terminusgps/settings/prod.py
@@ -36,6 +36,19 @@ USE_I18N = True
 USE_TZ = True
 WIALON_API_TOKEN = os.getenv("WIALON_API_TOKEN")
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / "debug.log",
+        }
+    },
+    "loggers": {"django": {"handlers": ["file"], "level": "DEBUG", "propagate": True}},
+}
+
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
     "staticfiles": {
