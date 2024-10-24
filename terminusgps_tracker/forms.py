66064197f83caa79d5c281c5374aad25a4d7fb65
darthnall <blake@proton.me>
2024-10-24 15:56:51 -0500

    Settled on form rendering set-up

--- terminusgps_tracker/forms.py
+++ terminusgps_tracker/forms.py
@@ -1,10 +1,10 @@
-from typing import Any
+from typing import Any, Collection
 
 from django import forms
+from django.forms import widgets
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.forms.renderers import TemplatesSetting
-from django.template.base import Template
 from django.utils.translation import gettext_lazy as _
 
 from terminusgps_tracker.validators import (
@@ -14,29 +14,73 @@ from terminusgps_tracker.validators import (
     validate_wialon_unit_name,
     validate_wialon_username,
 )
-from terminusgps_tracker.widgets import AddressField
+
+
+class TerminusTextInput(widgets.TextInput):
+    template_name = "terminusgps_tracker/forms/widgets/text.html"
+
+
+class TerminusSelectInput(widgets.Select):
+    template_name = "terminusgps_tracker/forms/widgets/select.html"
+
+
+class TerminusInput(widgets.Input):
+    template_name = "terminusgps_tracker/forms/widgets/input.html"
+
+
+class TerminusNumberInput(widgets.NumberInput):
+    template_name = "terminusgps_tracker/forms/widgets/number.html"
+
+
+class CreditCardWidget(forms.MultiWidget):
+    template_name = "terminusgps_tracker/forms/widgets/multiwidget.html"
+
+    def decompress(self, value: dict[str, Any]) -> list:
+        data_keys = ["number", "expiry", "ccv"]
+        if value:
+            return [value.get(key) for key in data_keys]
+        return [None] * len(data_keys)
+
+
+class AddressWidget(forms.MultiWidget):
+    template_name = "terminusgps_tracker/forms/widgets/multiwidget.html"
+
+    def decompress(self, value: dict[str, Any]) -> list:
+        data_keys = ["street", "city", "state", "zip", "country", "phone"]
+        if value:
+            return [value.get(key) for key in data_keys]
+        return [None] * len(data_keys)
+
+
+class CreditCardField(forms.MultiValueField):
+    def compress(self, data_list: Collection) -> dict[str, Any]:
+        data_keys = ["number", "expiry", "ccv"]
+        if data_list:
+            return dict(zip(data_keys, data_list))
+        return dict(zip(data_keys, [None] * len(data_keys)))
+
+
+class AddressField(forms.MultiValueField):
+    def compress(self, data_list: Collection) -> dict[str, Any]:
+        data_keys = ["street", "city", "state", "zip", "country", "phone"]
+        if data_list:
+            return dict(zip(data_keys, data_list))
+        return dict(zip(data_keys, [None] * 6))
 
 
 class TerminusFormRenderer(TemplatesSetting):
     form_template_name = "terminusgps_tracker/forms/form.html"
     field_template_name = "terminusgps_tracker/forms/field.html"
 
-    def get_template(self, template_name: str) -> Template | None:
-        print(f"Getting template '{template_name}'...")
-        return super().get_template(template_name)
-
 
 class CountryCode(models.TextChoices):
-    UNITED_STATES = "US", _("United States")
-    CANADA = "CA", _("Canada")
-    MEXICO = "MX", _("Mexico")
-
-
-class CreditCardUploadForm(forms.Form):
-    address = AddressField()
+    US = "US", _("United States")
+    CA = "CA", _("Canada")
+    MX = "MX", _("Mexico")
 
 
 class CustomerRegistrationForm(forms.Form):
+    default_renderer = TerminusFormRenderer
     first_name = forms.CharField(label="First Name", min_length=4, max_length=64)
     last_name = forms.CharField(label="Last Name", min_length=4, max_length=64)
     email = forms.EmailField(
@@ -84,6 +128,7 @@ class CustomerRegistrationForm(forms.Form):
 
 
 class AssetCustomizationForm(forms.Form):
+    default_renderer = TerminusFormRenderer
     asset_name = forms.CharField(
         label="Asset Name",
         validators=[validate_wialon_unit_name],
@@ -96,4 +141,46 @@ class AssetCustomizationForm(forms.Form):
         widget=forms.widgets.NumberInput(),
         min_length=15,
         max_length=24,
+    )
+
+
+class CreditCardUploadForm(forms.Form):
+    default_renderer = TerminusFormRenderer
+    credit_card = CreditCardField(
+        label="Credit Card",
+        require_all_fields=True,
+        fields=(
+            forms.CharField(label="Card Number"),
+            forms.CharField(label="Card Expiration"),
+            forms.CharField(label="Card CCV #"),
+        ),
+        widget=CreditCardWidget(
+            widgets={
+                "number": TerminusTextInput(),
+                "expiry": TerminusTextInput(),
+                "ccv": TerminusTextInput(),
+            }
+        ),
+    )
+    address = AddressField(
+        label="Address",
+        require_all_fields=False,
+        fields=(
+            forms.CharField(label="Street"),
+            forms.CharField(label="City"),
+            forms.CharField(label="State"),
+            forms.CharField(label="Zip"),
+            forms.ChoiceField(label="Country"),
+            forms.CharField(label="Phone #"),
+        ),
+        widget=AddressWidget(
+            widgets={
+                "street": TerminusTextInput(),
+                "city": TerminusTextInput(),
+                "state": TerminusTextInput(),
+                "zip": TerminusTextInput(),
+                "country": TerminusSelectInput(choices=CountryCode.choices),
+                "phone": TerminusTextInput(),
+            }
+        ),
     )
