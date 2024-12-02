6f912426807ac551de1d717aabb55e04df6b1d2e
darthnall <blake@proton.me>
2024-12-02 15:41:31 -0600

    Fixed merchant authentication

--- terminusgps_tracker/integrations/authorizenet/auth.py
+++ terminusgps_tracker/integrations/authorizenet/auth.py
@@ -4,4 +4,7 @@ from django.conf import settings
 
 
 def get_merchant_auth() -> merchantAuthenticationType:
-    return settings.MERCHANT_AUTHENTICATION
+    return merchantAuthenticationType(
+        name=str(settings.MERCHANT_AUTH_LOGIN_ID),
+        transactionKey=str(settings.MERCHANT_AUTH_TRANSACTION_KEY),
+    )
