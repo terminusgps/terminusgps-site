681ec3b903d3d3e7083257c641ee0c30fc702399
darthnall <blake@proton.me>
2024-12-02 14:37:01 -0600

    Removed reference to deleted signals

--- terminusgps_tracker/apps.py
+++ terminusgps_tracker/apps.py
@@ -5,6 +5,3 @@ class TerminusgpsTrackerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "terminusgps_tracker"
     verbose_name = "Terminus GPS Tracker"
-
-    def ready(self):
-        import terminusgps_tracker.signals
