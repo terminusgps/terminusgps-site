c1aa3754e335feaae07fcccfd87fb11115699fab
darthnall <blake@proton.me>
2024-12-02 12:32:09 -0600

    Updated aws

--- terminusgps/aws.py
+++ terminusgps/aws.py
@@ -2,7 +2,7 @@ from boto3.session import Session
 
 
 def get_secret(secret_name: str, region_name: str = "us-east-1") -> str:
-    client = Session(profile_name="pwuser").client(
+    client = Session(profile_name="admin").client(
         service_name="secretsmanager", region_name=region_name
     )
     secret = client.get_secret_value(SecretId=secret_name)["SecretString"]
