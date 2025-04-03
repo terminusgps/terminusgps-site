9322b0585e65722f3ce1e56260a602524ae67552
darthnall <blake@proton.me>
2025-04-03 13:21:18 -0500

    Updated templates and added customer actions

--- terminusgps_tracker/admin.py
+++ terminusgps_tracker/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin, messages
 from django.utils.translation import ngettext
+from terminusgps.wialon.items import WialonResource
+from terminusgps.wialon.session import WialonSession
 
 from terminusgps_tracker.models.customers import (
     Customer,
@@ -20,6 +22,8 @@ class CustomerAdmin(admin.ModelAdmin):
     actions = [
         "refresh_customer_payment_methods",
         "refresh_customer_shipping_addresses",
+        "block_customer_accounts",
+        "unblock_customer_accounts",
     ]
     fieldsets = [
         (None, {"fields": ["user"]}),
@@ -64,6 +68,92 @@ class CustomerAdmin(admin.ModelAdmin):
             % {"count": len(queryset)},
             messages.SUCCESS,
         )
+
+    @admin.action(description="Block selected customer accounts")
+    def block_customer_accounts(self, request, queryset):
+        results_map = {"success": [], "skipped": []}
+        with WialonSession() as session:
+            for customer in queryset:
+                if not customer.wialon_resource_id:
+                    results_map["skipped"].append(customer)
+                    continue
+
+                resource: WialonResource = WialonResource(
+                    id=customer.wialon_resource_id, session=session
+                )
+                if not resource.is_account:
+                    results_map["skipped"].append(customer)
+                    continue
+                else:
+                    resource.disable_account()
+                    results_map["success"].append(customer)
+
+        if results_map["skipped"]:
+            self.message_user(
+                request,
+                ngettext(
+                    "%(count)s customer account was not blocked.",
+                    "%(count)s customer accounts were not blocked.",
+                    len(results_map["skipped"]),
+                )
+                % {"count": len(results_map["skipped"])},
+                messages.WARNING,
+            )
+
+        if results_map["success"]:
+            self.message_user(
+                request,
+                ngettext(
+                    "%(count)s customer account was blocked.",
+                    "%(count)s customer accounts were blocked.",
+                    len(results_map["success"]),
+                )
+                % {"count": len(results_map["success"])},
+                messages.SUCCESS,
+            )
+
+    @admin.action(description="Unblock selected customer accounts")
+    def unblock_customer_accounts(self, request, queryset):
+        results_map = {"success": [], "skipped": []}
+        with WialonSession() as session:
+            for customer in queryset:
+                if not customer.wialon_resource_id:
+                    results_map["skipped"].append(customer)
+                    continue
+
+                resource: WialonResource = WialonResource(
+                    id=customer.wialon_resource_id, session=session
+                )
+                if not resource.is_account:
+                    results_map["skipped"].append(customer)
+                    continue
+                else:
+                    resource.enable_account()
+                    results_map["success"].append(customer)
+
+        if results_map["skipped"]:
+            self.message_user(
+                request,
+                ngettext(
+                    "%(count)s customer account was not unblocked.",
+                    "%(count)s customer accounts were not unblocked.",
+                    len(results_map["skipped"]),
+                )
+                % {"count": len(results_map["skipped"])},
+                messages.WARNING,
+            )
+
+        if results_map["success"]:
+            self.message_user(
+                request,
+                ngettext(
+                    "%(count)s customer account was unblocked.",
+                    "%(count)s customer accounts were unblocked.",
+                    len(results_map["success"]),
+                )
+                % {"count": len(results_map["success"])},
+                messages.SUCCESS,
+            )
 
 
 @admin.register(CustomerAsset)
