195f1bafc30f69e7988e4e4b81205cb927547a03
darthnall <blake@proton.me>
2025-04-08 14:07:53 -0500

    Updated qrcode generation

--- terminusgps_tracker/management/commands/qrcode.py
+++ terminusgps_tracker/management/commands/qrcode.py
@@ -2,6 +2,7 @@ import argparse
 
 import qrcode
 from django.core.management.base import BaseCommand
+from PIL import Image, ImageDraw, ImageFont
 
 
 class Command(BaseCommand):
@@ -37,14 +38,38 @@ class Command(BaseCommand):
 
         """
         for imei in options["imei_numbers"]:
-            self.generate_qr_code(str(imei))
+            qr = self.create_qr_code(str(imei))
+            img = self.draw_qr_code_text(qr, text=f"IMEI #: {imei}")
+            img.save(f"{imei}.png")
 
-    def generate_qr_code(self, imei_number: str) -> None:
+    def create_qr_code(self, imei_number: str) -> qrcode.QRCode:
         qr = qrcode.QRCode(
             version=1, error_correction=qrcode.ERROR_CORRECT_L, box_size=10, border=4
         )
         qr.add_data(
             f"{self.protocol}://{self.domain}/assets/create/?imei={imei_number}"
         )
-        img = qr.make_image(fill_color="black", back_color="white")
-        img.save(f"{imei_number}.png")
+        qr.make_image(fill_color="black", back_color="white")
+        return qr
+
+    def draw_qr_code_text(self, qr: qrcode.QRCode, text: str) -> Image:
+        img = qr.make_image(fit=True)
+        img = img.convert("RGB")
+        overlay = ImageDraw.Draw(img)
+
+        font = ImageFont.truetype(
+            "/usr/share/fonts/truetype/open-sans/OpenSans-Regular.ttf", 28
+        )
+        padding = 10
+
+        text_bbox = overlay.textbbox((0, 0), text, font=font)
+        text_w = text_bbox[2] - text_bbox[0]
+        text_h = text_bbox[3] - text_bbox[1]
+
+        img_w, img_h = img.size
+        x_pos = (img_w - text_w) / 2
+        y_pos = img_h - text_h - padding - 10
+
+        overlay.text((x_pos, y_pos), text, font=font, fill="black")
+
+        return img
