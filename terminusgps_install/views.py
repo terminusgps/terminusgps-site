b8aebd90087f8989adf64bb686ddec2a6f7a74a0
darthnall <blake@proton.me>
2025-04-10 15:34:23 -0500

    Updated views

--- terminusgps_install/views.py
+++ terminusgps_install/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import (
     DetailView,
@@ -9,64 +9,67 @@ from django.views.generic import (
 )
 from terminusgps.mixins import HtmxTemplateResponseMixin
 
-from terminusgps_install.forms import WialonAssetCreateForm
+from .forms import WialonAssetCreateForm, WialonAssetUpdateForm
+from .models import WialonAsset
 
 
-class InstallDashboardView(LoginRequiredMixin, HtmxTemplateResponseMixin, TemplateView):
+class UserIsStaffTestMixin(UserPassesTestMixin):
+    login_url = reverse_lazy("login")
+    permission_denied_message = "You must be staff in order to view this."
+    raise_exception = False
+
+    def test_func(self) -> bool:
+        return self.request.user.is_staff
+
+
+class InstallDashboardView(
+    UserIsStaffTestMixin, HtmxTemplateResponseMixin, TemplateView
+):
     content_type = "text/html"
     extra_context = {"title": "Dashboard"}
     http_method_names = ["get"]
-    template_name = "terminusgps_install/dashboard.html"
     partial_template_name = "terminusgps_install/partials/_dashboard.html"
-    login_url = reverse_lazy("login")
-    permission_denied_message = "You do not have permission to view this."
-    raise_exception = False
+    template_name = "terminusgps_install/dashboard.html"
 
 
-class InstallAssetCreateView(
-    PermissionRequiredMixin, HtmxTemplateResponseMixin, FormView
-):
+class InstallAssetCreateView(UserIsStaffTestMixin, HtmxTemplateResponseMixin, FormView):
     content_type = "text/html"
     extra_context = {"title": "Add Asset"}
     form_class = WialonAssetCreateForm
     http_method_names = ["get", "post"]
-    login_url = reverse_lazy("login")
+    model = WialonAsset
     partial_template_name = "terminusgps_install/assets/partials/_create.html"
-    permission_denied_message = "You do not have permission to view this."
-    raise_exception = False
     success_url = reverse_lazy("dashboard")
     template_name = "terminusgps_install/assets/create.html"
-    permission_required = "terminusgps_install.add_wialonasset"
 
 
-class InstallAssetDetailView(LoginRequiredMixin, HtmxTemplateResponseMixin, DetailView):
+class InstallAssetDetailView(
+    UserIsStaffTestMixin, HtmxTemplateResponseMixin, DetailView
+):
     content_type = "text/html"
     extra_context = {"title": "Asset Details"}
     http_method_names = ["get"]
-    login_url = reverse_lazy("login")
+    model = WialonAsset
     partial_template_name = "terminusgps_install/assets/partials/_detail.html"
-    permission_denied_message = "You do not have permission to view this."
-    raise_exception = False
     template_name = "terminusgps_install/assets/detail.html"
 
 
-class InstallAssetUpdateView(LoginRequiredMixin, HtmxTemplateResponseMixin, UpdateView):
+class InstallAssetUpdateView(
+    UserIsStaffTestMixin, HtmxTemplateResponseMixin, UpdateView
+):
     content_type = "text/html"
     extra_context = {"title": "Update Asset"}
+    form_class = WialonAssetUpdateForm
     http_method_names = ["get", "post"]
-    login_url = reverse_lazy("login")
+    model = WialonAsset
     partial_template_name = "terminusgps_install/assets/partials/_update.html"
-    permission_denied_message = "You do not have permission to view this."
-    raise_exception = False
     template_name = "terminusgps_install/assets/update.html"
 
 
-class InstallAssetListView(LoginRequiredMixin, HtmxTemplateResponseMixin, ListView):
+class InstallAssetListView(UserIsStaffTestMixin, HtmxTemplateResponseMixin, ListView):
     content_type = "text/html"
     extra_context = {"title": "Asset List"}
     http_method_names = ["get"]
-    login_url = reverse_lazy("login")
+    model = WialonAsset
     partial_template_name = "terminusgps_install/assets/partials/_list.html"
-    permission_denied_message = "You do not have permission to view this."
-    raise_exception = False
     template_name = "terminusgps_install/assets/list.html"
