880bc765b0a590285a0b61a25b324df8c5148241
darthnall <blake@proton.me>
2025-07-10 14:51:42 -0500

    Update emails

--- src/emails.py
+++ src/emails.py
@@ -7,19 +7,22 @@ from terminusgps_tracker.models import Customer
 
 
 def send_registration_email(customer: Customer) -> Customer:
+    context = {
+        "first_name": customer.user.first_name,
+        "login_link": f"https://app.terminusgps.com/login?{urlencode({'username': customer.user.email})}",
+    }
     text_content = render_to_string(
-        "terminusgps/emails/registration_complete.txt",
-        context={
-            "first_name": customer.user.first_name,
-            "login_link": f"https://app.terminusgps.com/login?{urlencode({'username': customer.user.email})}",
-        },
+        "terminusgps/emails/registration_complete.txt", context=context
     )
+    # html_content = render_to_string(
+    #     "terminusgps/emails/registration_complete.html", context=context
+    # )
     msg = EmailMultiAlternatives(
         subject="Terminus GPS - Account Registered",
         body=text_content,
-        from_email="support@terminusgps.com",
         to=[customer.user.email],
         bcc=["pspeckman@terminusgps.com", "blake@terminusgps.com"],
     )
+    # msg.attach_alternative(html_content, "text/html")
     msg.send(fail_silently=True)
     return customer
