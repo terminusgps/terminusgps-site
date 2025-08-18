b01fd7430f906ca801f062a98d76bf686d440cf0
darthnall <blake@proton.me>
2025-08-18 10:50:11 -0500

    Simplified emails

--- src/emails.py
+++ src/emails.py
@@ -7,24 +7,17 @@ from django.template.loader import render_to_string
 from terminusgps_tracker.models import Customer
 
 
-def get_admin_email_list() -> list[str]:
-    return [admin[1] for admin in settings.ADMINS]
-
-
-def send_registration_email(customer: Customer) -> Customer:
-    context = {
-        "first_name": customer.user.first_name,
-        "login_link": f"https://app.terminusgps.com/login?{urlencode({'username': customer.user.email})}",
-    }
-    text_content = render_to_string(
-        "terminusgps/emails/registration_complete.txt", context=context
+def send_registration_email(customer: Customer) -> None:
+    email_content = render_to_string(
+        "terminusgps/emails/registration_complete.txt",
+        context={
+            "first_name": customer.user.first_name,
+            "login_link": f"https://app.terminusgps.com/login?{urlencode({'username': customer.user.email})}",
+        },
     )
-    msg = EmailMultiAlternatives(
+    EmailMultiAlternatives(
         subject="Terminus GPS - Account Registered",
-        body=text_content,
+        body=email_content,
         to=[customer.user.email],
-        bcc=get_admin_email_list(),
-    )
-    msg.send(fail_silently=True)
-
-    return customer
+        bcc=[admin[1] for admin in settings.ADMINS],
+    ).send(fail_silently=True)
