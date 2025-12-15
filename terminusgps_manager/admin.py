840b3423a2da526d852b09366bd0f7c9c9909004
darthnall <blake@proton.me>
2025-12-15 14:12:03 -0600

    Updated admin

--- terminusgps_manager/admin.py
+++ terminusgps_manager/admin.py
@@ -8,7 +8,7 @@ class TerminusgpsCustomerAdmin(admin.ModelAdmin):
     readonly_fields = ["tax", "grand_total"]
     exclude = ["subscription"]
     fieldsets = [
-        (None, {"fields": ["user", "account"]}),
+        (None, {"fields": ["django_user", "wialon_user"]}),
         (
             "Pricing",
             {"fields": ["tax_rate", "subtotal", "tax", "grand_total"]},
