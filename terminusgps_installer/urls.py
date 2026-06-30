f987b1d106589856e5ed83627a7ae4e68a906e88
Blake Nall <blake@terminusgps.com>
2026-06-30 13:48:41 -0500

    Updated urlconf

--- terminusgps_installer/urls.py
+++ terminusgps_installer/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "terminusgps_installer"
 urlpatterns = [
     path("", views.home_view, name="home"),
-    path("jobs/", views.job_list_view, name="job list"),
+    path("jobs/list/", views.job_list_view, name="job list"),
     path("jobs/form/", views.new_job_form_view, name="new job form"),
     path(
         "jobs/<int:job_pk>/details/",
