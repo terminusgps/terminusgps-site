e05d002e642905eb2cf99b4d53d2d014e73e868c
Blake Nall <blake@terminusgps.com>
2026-07-15 11:46:15 -0500

    Added caching

--- terminusgps/settings/prod.py
+++ terminusgps/settings/prod.py
@@ -136,6 +136,13 @@ logging.config.dictConfig(
     }
 )
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+    }
+}
+
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
     "staticfiles": {
